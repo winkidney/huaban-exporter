720bc97b23f9ddb8b1dd0e962bbc295d604537c0
winkidney <winkidney@gmail.com>
2017-02-11 00:47:57 +0800

    Update: Add setup.py and console-entry named "huaban"

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+install_requires = (
+    "cmdtree",
+    "requests",
+    "tqdm",
+)
+
+setup(
+    name='huaban-exporter',
+    version='0.0.1',
+    py_modules="huaban_exporter.py",
+    install_requires=install_requires,
+    url='https://github.com/winkidney/huaban-exporter',
+    license='MIT',
+    author='winkidney',
+    author_email='winkidney@gmail.com',
+    description='Backup tool for huaban user.'
+                'Save pictures from huaban.',
+    entry_points={
+        'console_scripts': ['huaban=huaban_exporter:cmd.entry'],
+    }
+)
